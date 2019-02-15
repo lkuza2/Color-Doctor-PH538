71c95e611a540fbc3339c9c9e06a885cfee960b3
Luke Kuza <kuzalj@rose-hulman.edu>
2019-02-15 01:51:09 -0500

    * Move training set to "Train" folder instead of "Small" folder * Add introduction lines to program to explain to user how to use * Allow user to re-train model if it exists and auto-detect if a model needs to be trained * Allow user to use prompts to decide number of epochs and steps

--- main.py
+++ main.py
@@ -2,72 +2,54 @@ from __future__ import absolute_import, division, print_function
 
 # TensorFlow and tf.keras
 import os
-
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 # Helper libraries
 import numpy as np
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
-from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from skimage.color import rgb2lab, lab2rgb
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.callbacks import TensorBoard
 from tensorflow.python.keras.engine import InputLayer
 from tensorflow.python.keras.layers import Conv2D, UpSampling2D
 from tensorflow.python.keras.models import load_model
-from tensorflow.python.keras.models import save_model
-
-import h5py
 
 print(tf.__version__)
 
 
-def main2():
-    # Get images
-    image = img_to_array(load_img('woman.png'))
-    image = np.array(image, dtype=float)
-    # Import map images into the lab colorspace
-    X = rgb2lab(1.0 / 255 * image)[:, :, 0]
-    Y = rgb2lab(1.0 / 255 * image)[:, :, 1:]
-    Y = Y / 128
-    X = X.reshape(1, 400, 400, 1)
-    Y = Y.reshape(1, 400, 400, 2)
-    # Building the neural network
-    model = Sequential()
-    model.add(InputLayer(input_shape=(None, None, 1)))
-    model.add(Conv2D(8, (3, 3), input_shape=(None, None, 1), activation='relu', padding='same', strides=2))
-    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
-    # Finish model
-    model.compile(optimizer='rmsprop', loss='mse')
-    # Train the neural network
-    model.fit(x=X, y=Y, batch_size=1, epochs=3000)
-    print(model.evaluate(X, Y, batch_size=1))
-    # Output colorizations
-    output = model.predict(X)
-    output = output * 128
-    canvas = np.zeros((400, 400, 3))
-    canvas[:, :, 0] = X[0][:, :, 0]
-    canvas[:, :, 1:] = output[0]
-    plt.imsave("img_result.png", lab2rgb(canvas))
-    plt.imsave("img_gray_scale.png", rgb2gray(lab2rgb(canvas)))
-
-
 def main():
+    print("Welcome to Color Doctor v1.00 by Gavin Karr and Luke Kuza")
+    print('Usage: Put colored training images in "./color_images/Train"')
+    print('Put B&W images to colorize in "./gray_images/Test')
+    print("Searching for trained model...")
+    if Path("./result/network.h5").is_file():
+        result = input('network.h5 found! Use old model? Selecting "n" retrains the network (Y/n)')  # Python 3
+        if result.lower() == "n":
+            os.remove('./result/network.h5')
+            train_net_prompt()
+    else:
+        train_net_prompt()
+
+    print("Testing network...")
+    test_net()
+    print("Output is in folder ./result")
+
+
+def train_net_prompt():
+    steps = input('Enter steps per epoch (100): ')  # Python 3
+    epochs = input('Enter number of epochs (5): ')  # Python 3
+    print("Training network...")
+    train_net(int(steps), int(epochs))
+
+
+def train_net(steps, epochs):
     # Get images
     X = []
-    for filename in os.listdir('./color_images/Small/'):
-        X.append(img_to_array(load_img('./color_images/Small/' + filename)))
-        print(filename)
+    for filename in os.listdir('./color_images/Train/'):
+        X.append(img_to_array(load_img('./color_images/Train/' + filename)))
+        # print(filename)
     X = np.array(X, dtype=float)
     # Set up training and test data
     split = int(0.95 * len(X))
@@ -108,37 +90,29 @@ def main():
 
     # Train model
     TensorBoard(log_dir='/output')
-    model.fit_generator(image_a_b_gen(batch_size), steps_per_epoch=100, epochs=5)
+    model.fit_generator(image_a_b_gen(batch_size), steps_per_epoch=steps, epochs=epochs)
     # Test images
     Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
     Xtest = Xtest.reshape(Xtest.shape + (1,))
     Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
     Ytest = Ytest / 128
     print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
-    # Load black and white images
-    color_me = []
-    for filename in os.listdir('./gray_images/Test/'):
-        color_me.append(img_to_array(load_img('./gray_images/Test/' + filename)))
-    color_me = np.array(color_me, dtype=float)
-    color_me = rgb2lab(1.0 / 255 * color_me)[:, :, :, 0]
-    color_me = color_me.reshape(color_me.shape + (1,))
     model.save('./result/network.h5')
     del model
 
-def testNet(color_me):
+
+def test_net():
     # Test model
-    
+
     model = load_model('./result/network.h5')
-    
-    
+
     color_me = []
     for filename in os.listdir('./gray_images/Test/'):
         color_me.append(img_to_array(load_img('./gray_images/Test/' + filename)))
     color_me = np.array(color_me, dtype=float)
     color_me = rgb2lab(1.0 / 255 * color_me)[:, :, :, 0]
     color_me = color_me.reshape(color_me.shape + (1,))
-    
-    
+
     output = model.predict(color_me)
     output = output * 128
     # Output colorizations
@@ -150,5 +124,4 @@ def testNet(color_me):
 
 
 if __name__ == '__main__':
-    #main()
-    testNet('./gray_images/Test/gray_image7.png')
+    main()
